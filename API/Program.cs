4d35f34b4d0aecada41d3d9c53fb90a1e5dde670
Pascal Fritschi <pascal.fritschi@comitas.com>
2024-08-06 16:14:05 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 
+using API;
 using API.Extensions;
 
 
@@ -19,6 +20,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
