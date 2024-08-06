772c62e870ed3d9bbad5ed3845b1ba6ff3749d9b
Pascal Fritschi <pascal.fritschi@comitas.com>
2024-08-06 13:43:17 +0200

    End of Section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,23 +12,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
 
-private accountService = inject(AccountService);
-  
-   cancelRegister = output<boolean>();
+  private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
+
+  cancelRegister = output<boolean>();
   model: any = {}
 
-  register()  {
+  register() {
     this.accountService.register(this.model).subscribe({
-      next: response => 
-        {
-          console.log(response);
-          this.cancel();
-        },
-        error: error => console.log(error)
-    });
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => this.toastr.error(error.error)
+    })
+
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-  };
+  }
 }
